79b864f9d648ad857b425054975b67b479c7c170
lonely <3060900582@qq.com>
2026-05-23 06:35:01 +0800

    fix: Disable locale detection, force default en

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import { locales, defaultLocale } from "./src/lib/i18n";
 export default createMiddleware({
   defaultLocale,
   locales,
+  localeDetection: false, // 禁用自动语言检测，强制默认 en
 });
 
 export const config = {
